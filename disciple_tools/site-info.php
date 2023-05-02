aa041e6964737fdc56737ee6fb62e3d2e1855f1f
corsacca <corsacca@gmail.com>
2023-05-02 15:49:35 +0200

    Update Sources Sync

--- disciple_tools/site-info.php
+++ disciple_tools/site-info.php
@@ -13,7 +13,7 @@ add_filter( 'go_webform_options', function ( $params ) {
     $params['lists'][] = 'list_18';
     $params['contact_fields'] = $params['contact_fields'] ?? [];
     $params['contact_fields']['projects'] = [ 'values' => [ [ 'value' => 'disciple_tools' ] ] ];
-    $params['contact_fields']['sources'] = [ 'values' => [ [ 'value' => 'disciple_tools' ] ] ];
+    $params['contact_fields']['sources'] = [ 'values' => [ [ 'value' => 'dt_news' ] ] ];
     $params['contact_fields']['notes'] = [ 'Signed up for D.T News' ];
 
     return $params;
