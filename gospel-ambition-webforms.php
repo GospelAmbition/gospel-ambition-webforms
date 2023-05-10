8e1c05173381be08b2cc321c2dc720268787a05e
corsacca <corsacca@gmail.com>
2023-05-10 09:38:05 +0200

    register manage_dt Settings

--- gospel-ambition-webforms.php
+++ gospel-ambition-webforms.php
@@ -72,3 +72,11 @@ class GO_Webform_Context_Switcher {
 }
 
 add_action( 'after_setup_theme', [ 'GO_Webform_Context_Switcher', 'instance' ], 10 );
+
+register_activation_hook( __FILE__, function (){
+    // Confirm 'Administrator' has 'manage_dt' privilege. This is key in 'remote' configuration when Disciple.Tools theme is not installed.
+    $role = get_role( 'administrator' );
+    if ( !empty( $role ) ) {
+        $role->add_cap( 'manage_dt' ); // gives access to dt plugin options
+    }
+} );
