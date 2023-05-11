c1e5417945d63285de0923ab3d4e2259b7915f3b
corsacca <corsacca@gmail.com>
2023-05-11 14:56:20 +0200

    v2023.05.11

--- gospel-ambition-webforms.php
+++ gospel-ambition-webforms.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/GospelAmbition/gospel-ambition-webforms
  * Description: Gospel Ambition Webforms
  * Text Domain: gospel-ambition-webforms
- * Version:  2023.05.10
+ * Version:  2023.05.11
  * Author URI: https://github.com/GospelAmbition/gospel-ambition-webforms
  * GitHub Plugin URI: https://github.com/GospelAmbition/gospel-ambition-webforms
  * Requires at least: 4.7.0
