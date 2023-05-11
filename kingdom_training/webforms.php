2d0e4cbb8dc1762e5dfd52023d0d506ea2fb7800
corsacca <corsacca@gmail.com>
2023-05-11 14:28:11 +0200

    add kt news webform hook

---
+++ kingdom_training/webforms.php
@@ -0,0 +1,12 @@
+<?php
+
+add_action( 'dt_webform_field', function ( $key ){
+    if ( $key === 'kt-news' ){
+        $values = [
+            'add_to_mailing_list_24' => 'Sign up for Kingdom.Training news and opportunities, and occasional communication from GospelAmbition.org',
+        ];
+
+        go_display_tag_fields( 'tags', $values );
+    }
+
+});
