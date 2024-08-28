398d4587c3ae003e5e736ec11f9753d732d992a0
corsacca <corsacca@gmail.com>
2024-08-28 13:37:06 +0100

    Prayer.Tools instead of P4M

--- prayer_global/webforms.php
+++ prayer_global/webforms.php
@@ -3,7 +3,7 @@
 add_action( 'dt_webform_field', function ( $key ){
     if ( $key === 'p4m-news' ){
         $values = [
-            'add_to_mailing_list_23' => 'Sign up for Pray4movement news and opportunities, and occasional communication from GospelAmbition.org',
+            'add_to_mailing_list_23' => 'Sign up for Prayer.Tools news and opportunities, and occasional communication from GospelAmbition.org',
         ];
 
         go_display_tag_fields( 'tags', $values );
