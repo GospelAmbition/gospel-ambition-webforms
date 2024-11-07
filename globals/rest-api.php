11de40e5e3acdc241d9fd19ca0efba687ca9f9b7
corsacca <corsacca@gmail.com>
2024-11-07 11:20:28 +0100

    update option name

--- globals/rest-api.php
+++ globals/rest-api.php
@@ -119,7 +119,7 @@ class GO_Webforms_Endpoints
             return new WP_Error( 'cf_token', 'Invalid token', [ 'status' => 400 ] );
         }
         //secret key
-        $secret_key = get_option( 'dt_webform_cf_site_key', '' );
+        $secret_key = get_option( 'dt_webform_cf_secret_key', '' );
         if ( empty( $secret_key ) ){
             return new WP_Error( 'cf_token', 'Invalid token', [ 'status' => 400 ] );
         }
