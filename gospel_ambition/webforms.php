9068502935cd594247cf70f4b0b980f0a66dcb3e
corsacca <corsacca@gmail.com>
2025-05-27 15:49:38 +0100

    Build Go double optin email

--- gospel_ambition/webforms.php
+++ gospel_ambition/webforms.php
@@ -84,3 +84,25 @@ add_filter( 'dt_webform_fields_before_submit', function ( $fields ){
 
     return $fields;
 } );
+
+function send_dt_optin_email( $params ){
+    //generate key
+    $key = wp_generate_password( 50, false );
+    //save params
+    update_option( 'go_webform_double_optin_' . $key, $params, false );
+
+
+    //send email with confirm link
+    $subject = 'Confirm subscription to Gospel Ambition News';
+    $message = 'Hi ' . ( $params['first_name'] ?? '' ) . ',<br><br>';
+    $message .= 'Please confirm your subscription to Gospel Ambition News by clicking the link below:<br><br>';
+    $message .= '<a href="' . home_url() . '/wp-json/go-webform/confirm?key=' . $key . '">Confirm Subscription</a>';
+    $message .= '<br><br>Thank you for subscribing to Gospel Ambition News.';
+    $message .= '<br><br>If you did not request this subscription, please ignore this email.';
+    $headers = array('Content-Type: text/html; charset=UTF-8');
+    wp_mail( $params['email'], $subject, $message, $headers );
+}
+
+add_action( 'send_double_optin_email', function ( $params ){
+    send_dt_optin_email( $params );
+}, 10, 1 );
